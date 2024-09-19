154928597268d8b9bfeedfe55bba6a3adbbb05b3
amanmandal042 <vihapiraha16@gmail.com>
2024-09-19 22:22:43 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 #Empieza La Creacion De Funciones
-$link = mysqli_connect("sql3.freemysqlhosting.net", "sql3490732", "2RWi2ZWNLh", "sql3490732");
+$link = mysqli_connect("sql10.freemysqlhosting.net", "sql10732274", "xqYkbpZalV", "sql10732274");
 
 function sendMessage($chatId, $message, $message_id)
 {
@@ -34,7 +34,7 @@ function SetAdmin($data)
         $file = fopen("admins.txt", "a+");
         fwrite($file, $data . PHP_EOL);
         fclose($file);
-$link = mysqli_connect("sql3.freemysqlhosting.net", "sql3490732", "2RWi2ZWNLh", "sql3490732");
+$link = mysqli_connect("sql10.freemysqlhosting.net", "sql10732274", "xqYkbpZalV", "sql10732274");
 $sql = "INSERT INTO PERSONS (userid, role, username, credits) VALUES( '$data', '[ADMIN]', '$username', '99999999') ";
 $result = mysqli_query($link, $sql);
 mysqli_close($link);
@@ -56,7 +56,7 @@ function SetPremium($data)
     }    
 function sendMessage1($message) {
        $message = urlencode($message);
-        $url = $GLOBALS[website]."/sendMessage?chat_id=-1001554610198&text=".$message."&parse_mode=HTML";
+        $url = $GLOBALS[website]."/sendMessage?chat_id=-1002335978287&text=".$message."&parse_mode=HTML";
         file_get_contents($url);
         
 }
